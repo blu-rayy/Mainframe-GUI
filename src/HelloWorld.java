394f6f7fbe7e057e0ee9e6e3d795b7dcb7591ca3
Kristian <202311645@fit.edu.ph>
2024-10-22 18:25:41 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        MainFrame main = new MainFrame();
+        main.setVisible(true);
+    }
+}
---
+++ src/MainFrame.java
@@ -0,0 +1,55 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MainFrame extends JFrame implements ActionListener {
+
+    private final JMenuBar mb = new JMenuBar();
+
+    private final JMenu file = new JMenu("File");
+    private final JMenu about = new JMenu("About");
+    private final JMenu extra = new JMenu("Extra");
+
+    private final JMenuItem newItem = new JMenuItem("New File");
+    private final JMenuItem exitItem = new JMenuItem("Exit");
+    private final JMenuItem aboutItem = new JMenuItem("See Details");
+
+    public MainFrame(){
+
+        setTitle("Title of GUI");
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        // if no EXIT_ON_CLOSE it will be unable to close
+
+        mb.add(file);
+        mb.add(about);
+
+        about.add(aboutItem);
+        
+
+        file.setMnemonic('F'); // ALT + F to access F menu Item
+        exitItem.setMnemonic('X');
+        newItem.setMnemonic('N');
+
+        file.add(newItem);
+        file.add(extra);
+        file.addSeparator(); // highlight on selecting item
+        file.add(exitItem);
+
+        setJMenuBar(mb);
+
+        exitItem.addActionListener(this);
+        newItem.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource() == exitItem){
+            dispose(); //exits program
+        }
+        else if(e.getSource() == newItem){
+            System.out.println("New File Opened");
+            JOptionPane.showMessageDialog(this, "You have Created a New Item");
+        }
+    }
+}
